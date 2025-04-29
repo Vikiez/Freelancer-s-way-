1acc474eb9fbad3cee3489cb51874ab79f7eb91b
Vikiez <121822471+Vikiez@users.noreply.github.com>
2025-04-29 19:59:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// function testFunction() {
+//     alert('JS работает!');
+//     return true;
+// }
+let hamb = document.querySelector(".header__burger");
+let navMenu = document.querySelector(".header__list");
+
+hamb.addEventListener("click", mobileMenu);
+
+
+function mobileMenu() {
+   hamb.classList.toggle("active");
+   navMenu.classList.toggle("active");
+}
+
+// const navLink = document.querySelectorAll(".header__link");
+
+// navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+// function closeMenu() {
+//     hamb.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }
